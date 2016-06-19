72482de90567149534589ca342cd8bd0d6089675
高橋礼 <asirei1012@gmail.com>
2016-06-19 15:39:10 +0900

    syateki

---
+++ syateki/Assets/SyatekiControllerRight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyatekiControllerRight : MonoBehaviour {
+
+	public GameObject bullet;
+	public GameObject RightController;
+	public GameObject Gun;
+
+	public float speed = 1000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SteamVR_TrackedObject trackedObject = GetComponent<SteamVR_TrackedObject>();
+		var device = SteamVR_Controller.Input((int) trackedObject.index);
+		if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Shoot();			
+		}
+	}
+
+	void Shoot () {
+		GameObject obj = GameObject.Instantiate(bullet)as GameObject;
+		obj.transform.position = RightController.transform.position;
+		Vector3 force;
+		force = this.gameObject.transform.forward * speed;
+	}
+}
