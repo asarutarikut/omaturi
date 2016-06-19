95397548da0aeb9dc7ae926099371e8757d5f4b0
AkiyamaMasanori <asarutarikut@AkiyamaMasanori-no-MacBook-Air.local>
2016-06-19 13:13:25 +0900

    add wanage

---
+++ syateki/Assets/movePlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += transform.forward * speed * Time.deltaTime;
+	}
+}
