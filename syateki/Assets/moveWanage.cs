11faf2769bf2d3948b1af5f082d994e4b1f45153
AkiyamaMasanori <asarutarikut@AkiyamaMasanori-no-MacBook-Air.local>
2016-06-19 15:24:01 +0900

    add move

---
+++ syateki/Assets/moveWanage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class moveSyateki : MonoBehaviour {
+	public GameObject player;
+
+	// Update is called once per frame
+	void Update () {
+		float c = player.transform.position.x;
+		float d = player.transform.position.z;
+		float a = transform.position.x;
+		float b = transform.position.z;
+
+		if (Mathf.Abs (Mathf.Sqrt (Mathf.Pow (c - a, 2f)) + Mathf.Pow (d - b, 2f)) < 1.5) {
+			SceneManager.LoadScene ("Wanage");
+		}
+	}
+}
